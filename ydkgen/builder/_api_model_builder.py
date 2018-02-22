b775a3d0add64683e0c2accfec63d27b80496ea8
Abhi Keshav <abhirame@cisco.com>
2018-02-22 11:33:44 -0800

    Fix generator for enum key

--- ydkgen/builder/_api_model_builder.py
+++ ydkgen/builder/_api_model_builder.py
@@ -197,6 +197,8 @@ class ApiModelBuilder(object):
             parent_element.owned_elements.append(enum_class)
             enum_class.owner = parent_element
             prop.property_type = enum_class
+            enum_type.i_enum = enum_class
+            enum_type.parent.i_enum = enum_class
         elif bits_type is not None and bits_type == stmt.search_one('type'):
             # we have to create the specific subclass of FixedBitsDict
             bits_class = Bits(self.iskeyword)
@@ -217,6 +219,7 @@ class ApiModelBuilder(object):
                         parent_element.owned_elements.append(enum_class)
                         enum_class.owner = parent_element
                         contained_enum_type.i_enum = enum_class
+                        contained_enum_type.parent.i_enum = enum_class
 
                     if contained_bits_type is not None and contained_bits_type == contained_type:
                         bits_class = Bits(self.iskeyword)
@@ -555,6 +558,7 @@ class GroupingClassApiModelBuilder(ApiModelBuilder):
                     enum_class.owner = parent_element
                     prop.property_type = enum_class
                     enum_type.parent.i_enum = enum_class
+                    enum_type.i_enum = enum_class
             elif bits_type is not None:
                 if bits_type == stmt.search_one('type'):
                     # we have to create the specific subclass of FixedBitsDict
